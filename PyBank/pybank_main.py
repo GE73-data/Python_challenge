0738fefdf44b0ac12c2ea5be02cb73a48436c0cd
GE73-data <gloria.evans73@yahoo.com>
2021-04-08 20:19:55 -0400

    adding files and folders from homework instructions

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -1,32 +1,88 @@
-#TODO pseudo code befor I begin
+#Part 1 import modules / dependencies
+import csv
+import os
 
-# import modules / dependencies
-import csv   #read csv file using .reader methong
-import os   #os to join elements of the file path for PC
+csv_file_path = os.path.join(".", "Resources", "budget_data.csv")
 
-csv_file_path = os.path.join(",", "Resources", "budget_date.csv")
+file_to_output = os.path.join("Analysis", "Budgetanalysis.txt")
 
-file_to_output = os.path.join("Analysis", "test.txt")
-# print(csv_file_path)
+total_months = 0 
+total_profit_losses = 0
+value = 0
+average_change = 0
+count = 0
+greatest_increase = ["", 0]  #positive change
+greatest_decrease = ["", 0]  #negative change
+dates = []
+profits = []
+monthly_change = 0
 
-#Initialize variables
 
+#opening csv_file
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+
+    #Read the header
+    csv_header = next(csv_reader)
+
+    #read first row
+    column_names = next(csv_reader)
+    total_months += 1
+    total_profit_losses += int(column_names[1])
+    value = int(column_names[1])
+
+    #go thru each row
+    for row in csv_reader:
+        count = count + 1
+
+        #keep track of dates
+        dates.append(row[0])
+
+
+        #change & then add to the list of changes
+        average_change = int(row[1])-value
+        profits.append(average_change)
+        value = int(row[1])
+
+        
+        #number of months
+        total_months += 1
+
+        #total profit losses
+        total_profit_losses = total_profit_losses + int(row[1])
+
+    #Increase in profits
+        greatest_increase = max (profits)
+        greatest_index = profits.index(greatest_increase)
+        greatest_date = dates[greatest_index]
+
+    #Decrease in profits
+        greatest_decrease = min (profits)
+        decrease_index = profits.index(greatest_decrease)
+        decrease_date = dates[decrease_index]
+
+    #Average change in profit losses
+        average_change = sum(profits)/len(profits)
+       
 #......................................................................
-#Open and read the csv_file called budget_data.csv
-# with open("Resources/budget_data.csv","r") as budgetDatacsv:
-#     reader = csv.reader(budgetDatacsv)
-    # column_names = next(reader)
-    # total_months = 0
 
-    #Part 2 make calculations
-#     for row in reader:
-#         print(row)
-#         total_months = total_months + 1
-#         print(total_months)
-# #Part 3 save calculations to a txt file
-# output=(
-#     f'Total_Months = {total_months}'
-#     )
+#Create summary table
+print("........................................")
+print("Financial Analysis")   
+print("...........................................")
+print(f"Total Months: {str(total_months)}")
+print(f"Total profit losses: ${str(total_profit_losses)}")
+print(f"Average Change: ${str(round(average_change,2))}")
+print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+print(f"Greatest Decrease in Profits: {decrease_date} (${str(greatest_decrease)})")
 
-# with open("Budgetanalysis.txt","w") as budgetDatatxt:
-#     budgetDatatxt.write(output)
+
+with open (file_to_output, "w") as text:
+
+    text.write(f"Financial Analysis \n")
+    text.write(f"........................................... \n")
+    text.write(f"Total Months: {str(total_months)} \n")
+    text.write(f"Total profit losses: ${str(total_profit_losses)} \n")
+    text.write(f"Average Change: ${str(round(average_change,2))} \n")
+    text.write(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)}) \n")
+    text.write(f"Greatest Decrease in Profits: {decrease_date} (${str(greatest_decrease)}) \n")
--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -1,1 +1,52 @@
-#TODO pseudo code before I begin
+#Import modules
+import csv
+import os
+
+csv_file_path = os.path.join(".", "Resources", "election_data.csv")
+
+file_to_output = os.path.join("Analysis", "Electionanalysis.txt")
+
+#Variables
+total_votes = 0 
+count = 0
+percentage_votes = []
+candidates_votes = {}
+votes = []
+
+#opening csv_file
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+
+    #Read the header
+    csv_header = next(csv_reader)
+
+     #go thru each row
+    for row in csv_reader: 
+        candidate_name = row[2]
+        total_votes += 1
+
+        #Votes by Person
+        if candidate_name in candidates_votes.keys():
+            candidates_votes[candidate_name] += 1
+                    
+        else:
+            candidates_votes[candidate_name] = 1
+            
+
+   #......................................................................
+output_list = []
+#Create summary table
+output_list.append("Election Results \n")
+output_list.append("........................................ \n")
+output_list.append(f"Total votes: {str(total_votes)} \n")
+output_list.append("....................................... \n")
+for candidate in candidates_votes:
+    candidate_percentage = round(candidates_votes[candidate]/total_votes * 100, 2)
+    output_list.append(f"{candidate}: {candidate_percentage}% ({candidates_votes[candidate]}) \n") 
+output_list.append("..................................... \n")
+output_list.append(f"Winner: Khan")
+#to text file
+with open(file_to_output, "w") as text:
+    for item in output_list:
+        print(item) 
+        text.write(item)
